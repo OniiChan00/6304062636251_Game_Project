888721d13d71bb8405f8383785f06694a0d1c2b8
PHoomchanok <s6304062636251@email.kmutnb.ac.th>
2021-10-18 19:42:53 +0700

    Create WorldContactLisnener.java

---
+++ core/src/ToolCreateForCreateWorld/WorldContactLisnener.java
@@ -0,0 +1,50 @@
+package ToolCreateForCreateWorld;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.physics.box2d.Contact;
+import com.badlogic.gdx.physics.box2d.ContactImpulse;
+import com.badlogic.gdx.physics.box2d.ContactListener;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.Manifold;
+
+import Sprites.Interactiveobject;
+
+public class WorldContactLisnener implements ContactListener{
+
+	@Override
+	public void beginContact(Contact contact) {
+		Fixture fixA = contact.getFixtureA();
+		Fixture fixB = contact.getFixtureB();
+		
+		if(fixA.getUserData() == "head" || fixB.getUserData() == "head")
+		{
+			Fixture head = fixA.getUserData() == "head" ? fixA : fixB;
+			Fixture object = head == fixA ? fixB: fixA;
+			
+			if(object.getUserData() != null && Interactiveobject.class.isAssignableFrom(object.getUserData().getClass()))
+			{
+				((Interactiveobject) object.getUserData()).Headhit();
+			}
+		}
+		
+	}
+
+	@Override
+	public void endContact(Contact contact) {
+		
+		
+	}
+
+	@Override
+	public void preSolve(Contact contact, Manifold oldManifold) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void postSolve(Contact contact, ContactImpulse impulse) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
