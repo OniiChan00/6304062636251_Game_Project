e983e2de89372ade058d8bb1bf6e5275667d30fb
PHoomchanok <s6304062636251@email.kmutnb.ac.th>
2021-10-18 19:42:43 +0700

    Update block.java

--- core/src/Sprites/block.java
+++ core/src/Sprites/block.java
@@ -1,6 +1,7 @@
 
 package Sprites;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.physics.box2d.World;
@@ -10,7 +11,13 @@ public class block extends Interactiveobject{
 	public block(World world, TiledMap map, Rectangle bound) 
 	{
 		super(world, map, bound);
+		fixture.setUserData(this);
+	}
 
+	@Override
+	public void Headhit() {
+		Gdx.app.log("block","Collision");
+		
 	}
 	
 
